1be25ecf73fd89dd06d9261040cff19bb3610dc8
Paweł Rubin <pawelrubin19@gmail.com>
2020-03-05 13:13:29 +0100

    refactor

--- lista1/entropy_analyzer.py
+++ lista1/entropy_analyzer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3.8
 import math
 import os
 import sys
@@ -14,8 +14,7 @@ class EntropyAnalyzer:
         cond_stats: Dict[(int, int), int] = defaultdict(int)
 
         with open(filename, "br") as file:
-            file_bytes = file.read()
-            for i, c in enumerate(file_bytes):
+            for i, c in enumerate(file_bytes := file.read()):
                 stats[c] += 1
                 cond_stats[(c, file_bytes[i - 1] if i > 0 else 0)] += 1
 
@@ -23,16 +22,17 @@ class EntropyAnalyzer:
         self.entropy = 0
         self.cond_entropy = 0
 
-        for k, v in stats.items():
-            v_log = math.log2(v)
-            self.entropy += -(v_log - size_log) * v
-            for kk, vv in cond_stats.items():
-                if kk[0] == k:
-                    self.cond_entropy += vv * -(math.log2(vv) - v_log)
-        
+        for curr_byte, curr_byte_count in stats.items():
+            cb_log = math.log2(curr_byte_count)
+            self.entropy += (size_log - cb_log) * curr_byte_count
+            for bytes_pair, bytes_pair_count in cond_stats.items():
+                if bytes_pair[0] == curr_byte:
+                    self.cond_entropy += bytes_pair_count * (
+                        cb_log - math.log2(bytes_pair_count)
+                    )
+
         self.entropy /= size
         self.cond_entropy /= size
-
 
     def print(self) -> None:
         print(f"Results for {self.filename}")
